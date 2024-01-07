736c986156dd87d0d6c2f37a5c7b4630789fc627
Faiza1st <faixa.faisal@gmail.com>
2024-01-07 22:24:44 +1100

    Added changes to the colorcode determined by past, present and future

--- Assest/JavaScript/scrpit.js
+++ Assest/JavaScript/scrpit.js
@@ -3,22 +3,21 @@ const currentDayElement = document.getElementById("currentDay");
 
 
 // Gets current date and prints it on the brower 
-const timeNow = Date.now();
-const currentDate = new Date(timeNow);
+const currentDate = Date.now();
 currentDayElement.textContent = currentDate;
-
 // Display current day in the format of date/ Month
 $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
 
 $(document).ready(function () {
     // Function to generate time blocks for standard business hours
-    function generateTimeBlocks() {
+    function TimeBlocks() {
         const container = $(".container-lg");
         if (container.length === 0) return;
 
         // Check if time blocks have already been generated
         if ($(".time-block").length === 0) {
+            // Let time be from 9am to 5pm. 
             for (let hour = 9; hour <= 17; hour++) {
                 const blockId = `hour-${hour}`;
                 const timeBlock = $(`
@@ -35,40 +34,44 @@ $(document).ready(function () {
         }
     }
 
-    // Function to update time block classes based on current time
+    // Function to update time block classes based on current time, Also to Highligh the time of the day, my adding gray, red or green (Each representing a meaning)
     function updateBlocks() {
         var currentHour = dayjs().hour();
 
         $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
+            // if else statement to determine what color-coded past, present and future 
             if (blockHour < currentHour) {
+                // if the blockedHour is less than current time, remove the present and future class and add pass class to it. Which changes the block to gray color. 
                 $(this).removeClass("present future").addClass("past");
             } else if (blockHour === currentHour) {
+                // if the blockhour is equal to the current time, remove the past and future class and add the present class to it. Which changes the block color to red. 
                 $(this).removeClass("past future").addClass("present");
             } else {
+                // else if the time has not passed yet, remove the past and present class and add the future call to it. Which makes is green. 
                 $(this).removeClass("past present").addClass("future");
             }
         });
     }
 
     // Generate time blocks initially
-    generateTimeBlocks();
+    TimeBlocks();
     updateBlocks();
 
     // Update time blocks on scroll
     $(window).on("scroll", function () {
-        // Check if user has scrolled to a certain position (e.g., halfway through the page)
+        // Check if user has scrolled to a certain position to display the changes in the Scheduler.
         var scrollPosition = $(window).scrollTop() + $(window).height();
         var documentHeight = $(document).height();
 
         if (scrollPosition >= documentHeight / 2) {
-            generateTimeBlocks();
+            TimeBlocks();
             updateBlocks();
         }
     });
 
-    // Update time blocks every minute
+    // Update time blocks every minute. This isn't a required code, however will be useful when determining the color code for past, present and future. 
     setInterval(updateBlocks, 60000); // Update every minute
 });
 
